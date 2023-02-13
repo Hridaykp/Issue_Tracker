119aaf714e14fc6b707843482a90cb9f8f58b458
Hriday Kumar Pranit <hridaykp21@gmail.com>
2023-02-13 23:53:48 +0530

    almost complete

---
+++ config/mongoose.js
@@ -0,0 +1,17 @@
+//require the library
+const mongoose = require("mongoose");
+
+// here we are using the MongoDB Url we defined in our ENV file
+const DB_URL = "mongodb://localhost:27017/Issue_Tracker";
+//connect to the database
+mongoose 
+      .connect(DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+  })
+    .then(() => {
+        console.log(`Successfuly connected to MongoDB!!!`);
+  })
+  .catch((err) => {
+        console.log(`Error connecting to database ${err}`);
+  });
